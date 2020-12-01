745cb183387b296e32549c66d56f99ebb2edbe09
unknown <s192298>
2020-12-01 13:00:36 +0900

    12/1　今日の作業　古藏

--- home.php
+++ home.php
@@ -1,1 +1,14 @@
-// ホーム画面
+<?php
+
+?>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>討論の杜</h1>
+    <h3>現在行われている討論</h3>
+    
+    <a href="">サインアウトする</a>
+    <a href="acount_setting">アカウントの設定</a>
+    
+</body>
