31a112185f6849eeb5740ab09fe6526b995fe9b0
unknown <s192298>
2020-12-02 16:32:55 +0900

    12/2 今日の進捗　古藏

--- login.php
+++ login.php
@@ -1,40 +1,40 @@
- <!-- ログイン画面 -->
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>討論の杜 | ログイン</title>
+  <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
+  <style>h1{text-align: center;}</style>
+</head>
+<body>
+  <h1>討論の杜へログイン、新規登録</h1>
+  <div id="firebaseui-auth-container"></div>
 
-session_start();
-?>
+  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-auth.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/ui/3.5.2/firebase-ui-auth__ja.js"></script>
+  <script src="./config.js"></script>
+  <script>
+    //----------------------------------------------
+    // Firebase UIの設定
+    //----------------------------------------------
+    var uiConfig = {
+        // ログイン完了時のリダイレクト先
+        signInSuccessUrl: 'http://localhost/kaihatsu/done.html',
 
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Firebase Web Quickstart</title>
-        <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
+        // 利用する認証機能
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID  //メール認証
+        ],
 
-        <!-- TODO: Add SDKs for Firebase products that you want to use
-             https://firebase.google.com/docs/web/setup#available-libraries -->
-        <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-auth.js"></script>
-    </head>       
-    <body>
-	<h1>討論の杜</h1>
-	<h2>ログイン</h2>
-        <div class="container">
-            <input id="txtEmail" type="email" placeholder="Email">
-            <input id="txtPassword" type="password" placeholder="Password">
-            <button id="btnLogin" class="btn btn-action">
-            log in
-            </button>
-            <button id="btnSignUp" class="btn btn-secondary">
-            Sign Up
-            </button>
-            <button id="btnLogout" class="btn btn-action hide">
-            Lod out
-            </button>
-        </div>
-        <script src="./firebaseauth.js"></script>
-		<p>アカウントをお持ちでない方は<a href="acount_create.php">コチラ</a></p>
-    </body>
+        // 利用規約のURL(任意で設定)
+        tosUrl: 'http://example.com/kiyaku/',
+        // プライバシーポリシーのURL(任意で設定)
+        privacyPolicyUrl: 'http://example.com/privacy'
+      };
+
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start('#firebaseui-auth-container', uiConfig);
+</script>
+</body>
 </html>
-
-
