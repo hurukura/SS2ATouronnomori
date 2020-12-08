78a1138f08610a96c2d8ce4f7d0488e5df99d94b
unknown <s192003>
2020-12-08 13:08:07 +0900

    12/8 青山　パラメーターの取得・受取

--- discussion.php
+++ discussion.php
@@ -1,174 +1,380 @@
-//討論画面
-
 <!--
 参考資料
-https://blog.katsubemakito.net/firebase/firestore_realtime_1
+https://qiita.com/taketakekaho/items/52b7c196ddbd4cb3c968
 -->
 <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>FireStore Chat</title>
-            <style>
-            #chatlog{ width:500px; height:300px; border:1px solid gray; overflow-y:scroll; }
-            #uname{ width:80px; float:left; margin-right:10px; padding-top:5px; text-align:center}
-            #msg{ width:330px; height:30px; margin-right:10px; font-size:12pt;}
-            #sbmt{ width:100px; height:30px; }
-        </style>
-</head>
-<body>
-  <h1>FireStore Chat</h1>
+<html>
 
-  <!-- 発言が表示される領域 -->
-  <ul id="chatlog"></ul>
+<head>
+ <meta charset="utf-8">
+ <title>FireStore Chat</title>
+ <style>
+  #uname {
+   width: 80px;
+   float: left;
+   margin-right: 10px;
+   padding-top: 5px;
+   text-align: center;
+  }
 
-  <!-- 入力フォーム -->
-  <form id="form1">
-    <div id="uname"></div>
-    <input type="text" id="msg"><button type="button" id="sbmt">送信</button>
-  </form>
+  #form1 {
+   width: 100%;
+   display: flex;
+   text-align: center;
+   position: fixed;
+   bottom: 0;
+   background-color: #ccc;
+  }
 
-  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-firestore.js"></script>
+  /* テキストエリア、送信ボタン④ */
+  #bms_send {
+
+   display: flex;
+   width: 100%;
+   height: 48px;
+   padding: 4px;
+  }
+
+  #bms_send_message {
+   width: calc(100% - 125px);
+   /*常に送信ボタンの横幅を引いたサイズに動的に計算*/
+   line-height: 16px;
+   height: 48px;
+   padding: 14px 6px 0px 6px;
+   /*文字がテキストエリアの中心になる様に隙間調整*/
+   border: 1px solid #ccc;
+   border-radius: 4px;
+   /*角丸*/
+   text-align: left;
+   /*文字を左寄せ*/
+   box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.2) inset;
+   /*内側に影を入れてテキストエリアらしくした*/
+   box-sizing: border-box;
+   /*paddingとborderの要素の高さと幅の影響をなくす（要素に高さと幅を含める）*/
+
+  }
+
+  #bms_send_btn {
+   width: 72px;
+   height: 48px;
+   font-size: 16px;
+   line-height: 3em;
+   margin-right: 10px;
+   float: right;
+   /*bms_sendに対して右寄せ*/
+   color: #fff;
+   font-weight: bold;
+   background: #bcbcbc;
+   text-align: center;
+   /*文字をボタン中央に表示*/
+   border: 1px solid #bbb;
+   border-radius: 4px;
+   /*角丸*/
+   box-sizing: border-box;
+   /*paddingとborderの要素の高さと幅の影響をなくす（要素に高さと幅を含める）*/
+  }
+
+  #bms_send_btn:hover {
+   background: #13178E;
+   /*マウスポインタを当てた時にアクティブな色になる*/
+   cursor: pointer;
+   /*マウスポインタを当てた時に、カーソルが指の形になる*/
+  }
+
+  #radio {
+   width: 100px;
+  }
+
+  #your_container {
+   height: 1000px;
+   width: 100%;
+  }
+
+
+  /* タイムライン部分③ */
+  #bms_messages {
+   overflow: auto;
+   /* スクロールを効かせつつ、メッセージがタイムラインの外に出ないようにする */
+   height: 100%;
+   /*テキストエリアが下に張り付く様にする*/
+   border-right: 1px solid #ddd;
+   border-left: 1px solid #ddd;
+   background-color: #eee;
+   box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.2) inset;
+   /*ヘッダーの下に影を入れる*/
+  }
+
+
+  /* メッセージ全般のスタイル */
+  .bms_message {
+   margin: 0px;
+   padding: 0px 15px;
+   /*吹き出しがタイムラインの側面にひっつかない様に隙間を開ける*/
+   font-size: 16px;
+   word-wrap: break-word;
+   /* 吹き出し内で自動で改行 */
+   white-space: normal;
+   /*指定widthに合わせて、文字を自動的に改行*/
+  }
+
+  .bms_message_text {
+   font-size: 24px;
+  }
+
+  .bms_message_name {
+   font-size: 16px;
+   padding: 0px 0px 5px;
+  }
+
+  .bms_message_box {
+   margin-top: 20px;
+   /*上下の吹き出しがひっつかない様に隙間を入れる*/
+   max-width: 100%;
+   /*文字が長くなった時に吹き出しがタイムラインからはみ出さない様にする*/
+   font-size: 16px;
+  }
+
+  .bms_message_content {
+   padding: 10px;
+   /*文字や画像（コンテンツ）の外側に隙間を入れる*/
+  }
+
+  /* メッセージ１（左側） */
+  .bms_left {
+   float: left;
+   /*吹き出しをbms_messagesに対して左寄せ*/
+   line-height: 1.3em;
+  }
+
+  .bms_left .bms_message_box {
+   color: #fff;
+   /*テキストを黒にする*/
+   background: #888;
+   border: 2px solid #888;
+   border-radius: 15px 15px 15px 0px;
+   /*左下だけ尖らせて吹き出し感を出す*/
+   margin-right: 50px;
+   /*左側の発言だとわかる様に、吹き出し右側に隙間を入れる*/
+  }
+
+  /* メッセージ２（右側） */
+  .bms_right {
+   float: right;
+   /*吹き出しをbms_messagesに対して右寄せ*/
+   line-height: 1.3em;
+  }
+
+  .bms_right .bms_message_box {
+   color: #fff;
+   /*テキストを白にする*/
+   /*   background: #13178E;*/
+   background: #888;
+
+   border: 2px solid #888;
+   border-radius: 15px 15px 0px 15px;
+   /*右下だけ尖らせて吹き出し感を出す*/
+   margin-left: 50px;
+   /*右側の発言だとわかる様に、吹き出し左側に隙間を入れる*/
+  }
   
-  <script>
-	 var firebaseConfig = {
-    apiKey: "AIzaSyCceLxC6u7UkH5PNXtP57trfJYU-XB-NS8",
-    authDomain: "touronnpmori-sample.firebaseapp.com",
-    databaseURL: "https://touronnpmori-sample.firebaseio.com",
-    projectId: "touronnpmori-sample",
-    storageBucket: "touronnpmori-sample.appspot.com",
-    messagingSenderId: "13166144664",
-    appId: "1:13166144664:web:6980840758f51f4336f4ed",
-    measurementId: "G-6NMEDD12HJ"
+  .bms_mine .bms_message_box {
+   background: #13178E;
+   border: 2px solid #13178E;
+  }
+
+  /* 回り込みを解除 */
+  .bms_clear {
+   clear: both;
+   /* 左メッセージと右メッセージの回り込み(float)の効果の干渉を防ぐために必要（これが無いと、自分より下のメッセージにfloatが影響する） */
+
+  }
+ </style>
+</head>
+
+<body>
+ <h1>FireStore Chat</h1>
+
+ <!-- 発言が表示される領域 -->
+ <div id="your_container">
+  <div id="bms_messages">
+  </div>
+ </div>
+
+ <!-- 入力フォーム -->
+ <form id="form1">
+  <div id="uname"></div>
+  <div id="bms_send">
+   <textarea id="bms_send_message"></textarea>
+   <div id="radio">
+    <input type="radio" name="flag" value="0">賛成
+    <br><input type="radio" name="flag" value="1">反対
+   </div>
+   <div id="bms_send_btn">送信</div>
+  </div>
+ </form>
+
+ <!-- 以下script -->
+ <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-firestore.js"></script>
+ <script>
+  var firebaseConfig = { // 共有アカウント
+   apiKey: "AIzaSyCKJKx0gUnD2qBB3cdyk_wZ8bJAwdeqFWw",
+   authDomain: "touronnomori.firebaseapp.com",
+   databaseURL: "https://touronnomori.firebaseio.com",
+   projectId: "touronnomori",
+   storageBucket: "touronnomori.appspot.com",
+   messagingSenderId: "638781936294",
+   appId: "1:638781936294:web:32215a2b2d87370fb5da67",
+   measurementId: "G-F5WWPJ82Q4"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-    </script>
-  <script>
-    //---------------------------------------
-    // チャット初期処理
-    //---------------------------------------
-    // ユーザー名をランダムに決める
-    var uname = getUName();
-    document.getElementById("uname").innerHTML = uname;
-
-    // テキストボックスにfocus
-    // document.getElementById("msg").focus();
-
-    //---------------------------------------
-    // Firestoreの準備
-    //---------------------------------------
-    // Firestoreのインスタンス作成
-    var db = firebase.firestore();
-
-    // チャットルームのリファレンス取得
-    var messagesRef = db.collection("chatroom").doc("room1").collection("messages");
-
-    /**
-     * 同期処理
-     **/
-    messagesRef.orderBy("date", "asc").limit(20).onSnapshot( (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        // 追加
-        if ( change.type === 'added' ) {
-          addLog(change.doc.id, change.doc.data());
-        }
-        // 更新
-        else if( change.type === 'modified' ){
-          modLog(change.doc.id, change.doc.data());
-        }
-        // 削除
-        else if ( change.type === 'removed' ) {
-          removeLog(change.doc.id);
-        }
-      });
-    });
-
-    /**
-     * 送信ボタン押下
-     **/
-    document.getElementById("sbmt").addEventListener("click", ()=>{
-      let msg = document.getElementById("msg").value;
-      if( msg.length === 0 ){
-        return(false);
-      }
-      // メッセージをfirestoreへ送信
-      messagesRef.add({
-        name: uname,
-        msg: msg,
-        date: new Date().getTime()
-      })
-      .then(()=>{
-        let msg = document.getElementById("msg");
-        msg.focus();
-        msg.value = "";
-      })
-    });
-    // submitイベントは（いったん）無視する
-    document.getElementById("form1").addEventListener("submit", (e)=>{
-      e.preventDefault();
-    });
+ </script>
+ <script>
+  //---------------------------------------
+  // チャット初期処理
+  //---------------------------------------
+  // ユーザー名をランダムに決める
+  //★ユーザー名を表示させる
+  var uname = getUName();
+  document.getElementById("uname").innerHTML = uname;
 
 
-    /**
-     * ログに追加
-     */
-    function addLog(id, data){
-      // 追加するHTMLを作成
-      let log = `${data.name}: ${data.msg} (${getStrTime(data.date)})`;
-      let li  = document.createElement('li');
-      li.id   = id;
-      li.appendChild(document.createTextNode(log));
+  //---------------------------------------
+  // Firestoreの準備
+  //---------------------------------------
+  // Firestoreのインスタンス作成
+  var db = firebase.firestore();
 
-      // 表示エリアへ追加
-      let chatlog = document.getElementById("chatlog");
-      chatlog.insertBefore(li, chatlog.firstChild);
+  // チャットルームのリファレンス取得
+  //    ★home.phpで選択したルームIDを ここ↓  に入れる
+     var roomNo = location.search.substr(1,5);
+     console.log(roomNo);
+  var messagesRef = db.collection("chatroom").doc(roomNo).collection("messages");
+     
+  /**
+   * 同期処理
+   **/
+  messagesRef.orderBy("date", "asc").limit(20).onSnapshot((snapshot) => {
+   snapshot.docChanges().forEach((change) => {
+    // 追加
+    if (change.type === 'added') {
+     addLog(change.doc.id, change.doc.data());
     }
-
-    /**
-     * ログを更新
-     */
-    function modLog(id, data){
-      let log = document.getElementById(id);
-      if( log !== null ){
-        log.innerText = `${data.name}: ${data.msg} (${getStrTime(data.date)})`;
-      }
+    // 更新
+    else if (change.type === 'modified') {
+     modLog(change.doc.id, change.doc.data());
     }
-
-    /**
-     * ログを削除
-     **/
-    function removeLog(id){
-      let log = document.getElementById(id);
-      if( log !== null ){
-        log.parentNode.removeChild(log);
-      }
+    // 削除
+    else if (change.type === 'removed') {
+     removeLog(change.doc.id);
     }
+   });
+  });
+
+  /**
+   * 送信ボタン押下
+   **/
+  document.getElementById("bms_send_btn").addEventListener("click", () => {
+   let msg = document.getElementById("bms_send_message").value;
+   if (msg.length === 0) {
+    return (false);
+   }
+   let flag = document.getElementById("form1").flag.value;
+   if (flag.length === 0) {
+    console.log(flag);
+    return (false);
+   }
+   // メッセージをfirestoreへ送信
+   messagesRef.add({
+     name: uname,
+     msg: msg,
+     flag: flag,
+     date: new Date().getTime()
+    })
+    .then(() => {
+     let msg = document.getElementById("bms_send_message");
+     msg.focus();
+     msg.value = "";
+    })
+  });
+  // submitイベントは（いったん）無視する
+  document.getElementById("form1").addEventListener("submit", (e) => {
+   e.preventDefault();
+  });
 
 
-    /**
-     * ユーザー名をランダムに決定
-     **/
-     function getUName(){
-      let master = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"];
-      let i      = Math.floor( Math.random() * master.length );
-      return( master[i] );
+  /**
+   * ログに追加
+   */
+  function addLog(id, data) {
+   // 追加するHTMLを作成
+
+   if(data.name == uname){
+    if (data.flag == 0) {
+     var str = `<div class="bms_message bms_right bms_mine"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_name">${data.name}</div><div class="bms_message_text">${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
+    } else {
+     var str = `<div class="bms_message bms_left bms_mine"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_name">${data.name}</div><div class="bms_message_text">${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
     }
-
-    /**
-     * UNIX TIME => MM-DD hh:mm
-     **/
-    function getStrTime(time){
-      let t = new Date(time);
-      return(
-        ("0" + (t.getMonth() + 1)).slice(-2) + "-" +
-        ("0" + t.getDate()       ).slice(-2) + " " +
-        ("0" + t.getHours()      ).slice(-2) + ":" +
-        ("0" + t.getMinutes()    ).slice(-2)
-      );
+   }else{
+    if (data.flag == 0) {
+     var str = `<div class="bms_message bms_right"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_name">${data.name}</div><div class="bms_message_text">${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
+    } else {
+     var str = `<div class="bms_message bms_left"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_name">${data.name}</div><div class="bms_message_text">${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
     }
-  </script>
+   }
+   document.getElementById("bms_messages").innerHTML += str;
+  }
+
+  /**
+   * ログを更新
+   */
+  function modLog(id, data) {
+   let log = document.getElementById(id);
+   if (log !== null) {
+    log.innerText = `${data.name}: ${data.msg} (${getStrTime(data.date)})`;
+
+    if (data.flag == 0) {
+     var str = `<div class="bms_message bms_right"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text">${data.name}<br><br>${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
+    } else {
+     var str = `<div class="bms_message bms_left"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text">${data.name}<br><br>${data.msg} </div></div></div></div><div class="bms_clear"></div>`;
+    }
+   }
+  }
+
+  /**
+   * ログを削除
+   **/
+  function removeLog(id) {
+   let log = document.getElementById(id);
+   if (log !== null) {
+    log.parentNode.removeChild(log);
+   }
+  }
+
+
+  /**
+   * ユーザー名をランダムに決定
+   **/
+  function getUName() {
+   let master = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"];
+   let i = Math.floor(Math.random() * master.length);
+   return (master[i]);
+  }
+
+  /**
+   * UNIX TIME => MM-DD hh:mm
+   **/
+  function getStrTime(time) {
+   let t = new Date(time);
+   return (
+    ("0" + (t.getMonth() + 1)).slice(-2) + "-" +
+    ("0" + t.getDate()).slice(-2) + " " +
+    ("0" + t.getHours()).slice(-2) + ":" +
+    ("0" + t.getMinutes()).slice(-2)
+   );
+  }
+ </script>
 </body>
+
 </html>
--- home.php
+++ home.php
@@ -7,8 +7,20 @@
 </head>
     <style>
         
+        .discussion_table tr:hover td {
+	     background-color: #d9efff;
+           border-spacing: 8px 0px;
+             
+         }
+        
+        body{
+            background-image: url(images/background.jfif);
+        }
+        
+        
         h1{
             text-align: center;
+            font-family: "Century";
         }
         
         table {
@@ -16,13 +28,22 @@
             padding: 15px;
             border-collapse:separate;
             border-spacing: 0;
+             border-radius: 5px;
         }
         
         th{
             color: #fff;
             background: #008000;
             border-radius: 5px;
+            padding: 5px;
             
+        
+        }
+        td{
+             padding: 20px;
+             border-radius: 5px;
+             background: white;
+        }
         
     
     </style>
@@ -60,22 +81,70 @@
         //  firebase.initializeApp(firebaseConfig);
     </script>
     
+    <script type="text/javascript"><!--
+/*=============================================================
+マウスストーカー　
+===============================================================*/
+(function (){  //
+
+//★ハートの色指定。色を順番に"●",と区切って、いくつでも並べる。最後の]の前には,(カンマ)無し
+var cl=["#f0f","#f00","#e0f","#d07","#fff"];
+var n=20;           //ハートの数
+var msec=100;       //追従速度
+var hs=18;          //小さいハートの大きさ。単位px
+var wh=10;          //大きなハートの大きさ。原本10
+var af=0.5;         //ハートの透過度。原本0.8。0で完全透明なので0は指定しない
+
+/*★↓文字の影指定をCSS形式で記述。見本は赤のボカシ指定。好みで変更可。指定不要ならvar fcss="";で*/
+var fcss="text-shadow:0px 0px 10px #f00;";
+//指定ここまで----------------------------------------------
+function $(id){return document.getElementById(id);}/*document.getElementByIdを簡略化*/
+var i=0, xh, yh, mx=[],my=[];
+var len=cl.length;//追加
+
+var sss='';
+for(i=0;i<n;i++){
+var randomCl=cl[Math.floor(Math.random()*len)];//追加
+
+sss +="<span style='position:absolute; z_index:1; top:0;left:0;opacity:"+af+"; font-size:"+hs+"px;color:"+randomCl+"; "+fcss+"'id='Hearts"+i+"'>&hearts;</span>";} /*★&hearts;が特殊文字ハート*/
+document.write(sss);
+for (i=0; i<=n; i++) {mx[i]=-100; my[i]=-100; }
+document.onmousemove= function (e){mx[0]=e.pageX; my[0]=e.pageY;}
+function H_run(){
+for (i=n-1; i>=0; i--){
+if(i){mx[i] = mx[i-1];my[i] = my[i-1];}
+if(i<(n/2)){
+xh=mx[i]+wh*i*Math.cos(i*2*3.14/n+3.14/2);
+yh=my[i]-wh*i*Math.sin(i*2*3.14/n+3.14/2);
+}else{
+xh=mx[i]+wh*(n-i)*Math.cos(i*2*3.14/n+3.14/2);
+yh=my[i]-wh*(n-i)*Math.sin(i*2*3.14/n+3.14/2);
+}
+$("Hearts"+i).style.top=-(hs*2)+yh+"px"; $("Hearts"+i).style.left=-10+xh+"px";
+}
+}
+setInterval(H_run,msec);
+}());//即時関数終了
+// --></script>
+    
     <h1><img src="images/touron.png" width="550" height="300"style="display: block; margin: auto;"></h1>
     <h1>現在行われている討論</h1>
     <!-- <div id="maintable"></div>-->
-    <table id="roomtable" border="1" width="350" height="200">
+    <table class="discussion_table" id="roomtable" border="1" frame="void" width="400" height="250">
+        
+        
 
 
         <!--FireStoreからルーム情報を取得-->
         <script>
-            
+             
             
             //参加ボタンでトークに参加
             function discussion(id) {
                 var a = 1; 
                 ret = confirm("ルーム" + id + "に移動します");
                 if (ret == true) {
-                    location.href = "http://localhost/discussion.php";
+                    location.href = "http://localhost/discussion.php/?room" + id;
                 }
             }
             //観戦ボタンでトークを見る
@@ -83,7 +152,7 @@
                 var a = 1;
                 ret = confirm("ルーム" + id + "に移動します");
                 if (ret == true) {
-                    location.href = "http://localhost/discussion.php";
+                    location.href = "http://localhost/discussion.php/?room" + id + "par";
                 }
             }
 
@@ -103,7 +172,11 @@
 
 
                     //以下ルーム一覧表示
+                
+                
                     var str = "<tr><th>No.</th><th>お題</th><th>参加人数</th><th>参加/観戦</th></tr>";
+                
+                    
                     // tr部分のループ
                     for (var i = 0; i < room.length; i++) {
                         str += "<tr>";
@@ -127,9 +200,9 @@
                                     break;
                                 case 3: // 参加/観戦
                                     str += "<td align='center'>";
-                                    str += "<input type='button' ' name='button1' onclick='discussion(";
+                                    str += "<input type='button' ' name='chatroom' onclick='discussion(";
                                     str += i + 1;
-                                    str += ")' value='参加' /><input type='button' 'name='button2' onclick='participation(";
+                                    str += ")' value='参加' /><input type='button' 'name='kansenroom' onclick='participation(";
                                     str += i + 1;
                                     str += ")' value='観戦' />";
                                     str += "</td>";
@@ -151,4 +224,6 @@
     </table>
     <a href="">サインアウトする</a>
     <a href="acount_setting">アカウントの設定</a>
+    
+    
 </body>
